32886cf0ec71183b851fe8db193f338cf5affe21
Miguel Fonte <up202108888@up.pt>
2022-10-08 19:11:45 +0100

    Added Reading keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,9 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
     public Game() throws IOException {
         TerminalSize terminalSize = new TerminalSize(40, 20);
         DefaultTerminalFactory terminalFactory = new
@@ -20,16 +25,41 @@ public class Game {
         screen.setCursorPosition(null); // we don't need a cursor
         screen.startScreen(); // screens must be started
         screen.doResizeIfNecessary(); // resize screen if necessary
+
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException {
-        draw();
-    }
 
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if(KeyType.EOF==key.getKeyType()) break;
+
+        }
+    }
+    private void processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        if (key.getKeyType() == KeyType.ArrowUp){
+            y-=1;
+        }
+        else if (key.getKeyType() == KeyType.ArrowDown){
+            y+=1;
+        }
+        else if (key.getKeyType() == KeyType.ArrowLeft){
+            x-=1;
+        }
+        else if (key.getKeyType() == KeyType.ArrowRight){
+            x+=1;
+        }
+        if (key.getKeyType() == KeyType.Character && key.getCharacter()
+                == 'q'){
+            screen.close();
+        }
+    }
 
 }
