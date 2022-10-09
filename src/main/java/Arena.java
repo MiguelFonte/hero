4ada6831c9c41308488ea9e710ef1fd67a357a9c
Miguel Fonte <up202108888@up.pt>
2022-10-09 14:36:57 +0100

    Added coins

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -10,32 +10,40 @@ import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Arena {
     private int width;
     private int height;
     private List<Wall> walls;
+    private List<Coin> coins;
     private Hero hero = new Hero(10, 10);
 
     public Arena (int a,int b){
         width=a;
         height=b;
         this.walls = createWalls();
+        this.coins = createCoins();
 
     }
 
     public void processKey(KeyStroke key,Screen screen) throws IOException {
         if (key.getKeyType() == KeyType.ArrowUp){
             moveHero(hero.moveUp());
+            retrieveCoins();
         }
         else if (key.getKeyType() == KeyType.ArrowDown){
             moveHero(hero.moveDown());
+            retrieveCoins();
         }
         else if (key.getKeyType() == KeyType.ArrowLeft){
             moveHero(hero.moveLeft());
+            retrieveCoins();
         }
         else if (key.getKeyType() == KeyType.ArrowRight){
             moveHero(hero.moveRight());
+            retrieveCoins();
+
         }
         if (key.getKeyType() == KeyType.Character && key.getCharacter()
                 == 'q'){
@@ -61,6 +69,41 @@ public class Arena {
         return false;
 
     }
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        Position newPosition;
+        boolean canadd;
+        for (int i = 0; i < 5;) {
+            newPosition= new Position(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1);
+            canadd=true;
+            for(Coin coin : coins ){
+                if(coin.getPosition().equals(newPosition)){
+                    canadd=false;
+                    break;
+                }
+
+            }
+            if(hero.getPosition().equals(newPosition)){
+                canadd=false;
+            }
+            if (canadd){
+                coins.add(new Coin(newPosition.getX(), newPosition.getY()));
+                i++;
+            }
+
+        }
+        return coins;
+    }
+    public void retrieveCoins(){
+        for(Coin coin : coins){
+            if(coin.getPosition().equals(hero.getPosition())){
+                coins.remove(coin);
+                break;
+            }
+        }
+    }
+
     public void draw(TextGraphics graphics) {
 
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
@@ -69,6 +112,9 @@ public class Arena {
                 hero.draw("#222222", "X",graphics);
         for (Wall wall : walls)
             wall.draw("#111111","O",graphics);
+        for(Coin coin : coins){
+            coin.draw("#444444","O",graphics);
+        }
     }
 
     private List<Wall> createWalls() {
